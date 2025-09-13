10a253cda67544e79401a20960fbf11ecb1f47f3
mayelisanchez23-sketch <mayelisanchez23@gmail.com>
2025-09-12 19:06:43 -0600

    Update examen_del_primer_parcial.py

--- examen_del_primer_parcial.py
+++ examen_del_primer_parcial.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as pd
+import statistics
 
 # Importante: verifica que tu nombre y número de matrícula esten correctos
 
@@ -8,13 +8,16 @@ numero_de_matricula = 173030
 fecha = '2025-11-09'
 
 def capitalizacion():
-  capitalizacion = [17, 21, 44, 50, 79, 86, 140, 178, 203]
-  media = np.mean(capitalizacion)
-  mediana = np.median(capitalizacion)
-  moda = pd.Series(capitalizacion).mode()[0]
-  desv_est = np.std(capitalizacion)
-  return (media, mediana, moda, desv_est)
-
+    datos = [17, 21, 44, 50, 79, 86, 140, 178, 203]
+    media = np.mean(datos)
+    mediana = np.median(datos)
+    try:
+        moda = statistics.mode(datos)   
+    except statistics.StatisticsError:
+        moda = datos[0]  
+    desv_est = np.std(datos)
+    return (media, mediana, moda, desv_est)
+  
 def asistencia_dispersion():
     """
     La aistencia a los 10 últimos partidos en casa de las Águilas de Baltimore fue la siguiente:   
@@ -36,12 +39,14 @@ def histograma_np():
     """
     Nota: regrese el histograma generado con la función de numpy, no genere la gráfica
     """
-    calificaciones = [7.9, 7.8, 7.8, 6.7, 7.6, 8.7, 8.5, 7.3, 6.6, 9.9, 8.4, 7.2,
-                     6.6, 5.7, 9.4, 8.4, 7.2, 6.3, 5.1, 4.8, 5.0, 6.1, 7.1, 8.2,
-                     9.3, 10.0, 8.9]
-    counts, bins = np.histogram(calificaciones, bins=5)
-    
+    calificaciones = [
+        7.9, 7.8, 7.8, 6.7, 7.6, 8.7, 8.5, 7.3, 6.6, 9.9,
+        6.6, 5.7, 9.4, 8.4, 7.2, 6.3, 5.1, 4.8, 5.0, 6.1,
+        7.0, 9.3, 10.0, 8.9
+    ]
+    counts, bins = np.histogram(calificaciones, bins=np.arange(0, 11, 1))
     return counts, bins
+
 
 def correlacion():
     tamanio = np.array([100, 120, 140, 160, 180, 200, 220, 240, 260, 280])
